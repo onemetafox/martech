911093ecf9fa0753e9fb19722db9f18a516a66cf
James Coulter <jamesdcoulter1972@gmail.com>
2022-01-24 20:46:34 -0800

    update calendar style

--- Client/src/pages/calendar/Content.js
+++ Client/src/pages/calendar/Content.js
@@ -12,8 +12,40 @@ const Content = () =>{
     return(
         <Container maxWidth="lg">
             <Box sx={{ bgcolor: '#fff', height: '100%',width:'100%', marginTop:'30px', marginBottom:'30px', boxShadow:'0px 0px 30px 0px rgb(82 63 105 / 5%)'}}>
-                <Box sx={{p:4}}>
+                <Box
+                    sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    bgcolor: 'background.flex',
+                    backgroundColor:'#fff',
+                    justifyContent:"space-between",
+                    alignItems:"stretch",
+                    borderBlockEnd:'solid 1px #EBEDF3',
+                    height: '70px'
+                    }}
+                >
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            
+                            }}
+                    >
+                        
+                        <Box
+                            sx={{
+                                marginTop:'25px',
+                                marginLeft:'27px',
+                                fontSize: '1.275rem'
+                            }}
+                        >
+                            Team Calendar
+                        </Box>
+                    </Box>
+                </Box>
+                <Box sx={{padding:'2rem'}}>
                     <StrictMode>
+                        
                         <Calendar />
                     </StrictMode>
                 </Box>
--- Client/src/pages/calendar/calendar.js
+++ Client/src/pages/calendar/calendar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import events from "./events";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import "../../style/calendar.css";
 
 moment.locale("en-GB");
 const localizer = momentLocalizer(moment);
@@ -15,8 +15,6 @@ export default function ReactBigCalendar() {
   const [eventsData, setEventsData] = useState(events);
 
   const handleSelect = ({ start, end }) => {
-    console.log(start);
-    console.log(end);
     const title = window.prompt("New Event name");
     if (title)
       setEventsData([
