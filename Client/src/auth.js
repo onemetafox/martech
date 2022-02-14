f6ef368087976a9fc30890600138094a073c6800
James Coulter <jamesdcoulter1972@gmail.com>
2022-02-14 11:58:36 -0800

    remove unuse component in auth js file

--- Client/src/auth.js
+++ Client/src/auth.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
   Navigate, 
 } from "react-router-dom";
-import { toast, ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
 const PrivateRoute = (props) =>{
   var auth = false;
   const account = JSON.parse(sessionStorage.getItem('auth'));
